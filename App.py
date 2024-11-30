329b2e18f64d26daf696d2c622a53f4eea3726c3
unknown <74219426+yNeptun03@users.noreply.github.com.>
2024-11-30 13:46:43 +0100

    upload stracture of Python app

---
+++ App.py
@@ -0,0 +1,1 @@
+#Here is the place for the main code of App with  Sierpinski hexagon functions
---
+++ Main.py
@@ -0,0 +1,6 @@
+#This file run will the app
+import App
+
+if __name__ == "__main__":
+	app = App.App()
+	app.mainloop()
